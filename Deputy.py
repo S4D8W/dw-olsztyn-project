7365e89f1031388b3faea50f86a48c8cd2302c30
Igor Wieczorek <igorw645@gmail.com>
2020-02-04 21:53:13 +0100

    Pobiera mordy

--- Deputy.py
+++ Deputy.py
@@ -1,15 +1,16 @@
 
 class Deputy:
-    def __init__(self, xDaySelected, xList, xElectoralDistrict, xParliamentaryTraineeship, xClub, xDateAndPlaceOfBirth,xEducation,
-    xGraduatedSchool,xOccupation, xImageSrc, ImageBase64 ):
-        self.DaySelected = xDaySelected
-        self.List = xList
-        self.ElectoralDistrict = xElectoralDistrict
-        self.ParliamentaryTraineeship = xParliamentaryTraineeship
-        self.Club= xClub
-        self.DateAndPlaceOfBirth = xDateAndPlaceOfBirth
-        self.Education = xEducation
-        self.GraduatedSchool = xGraduatedSchool
-        self.Occupation = xOccupation
-        self.ImageSrc = xImageSrc
-        self.ImageBase64 = ImageBase64
+    def __init__(self, DaySelected, List, ElectoralDistrict,NumberOfVotes , ParliamentaryTraineeship, Club, DateAndPlaceOfBirth,Education,
+    GraduatedSchool,Occupation, ImageSrc, ImageArr ):
+        self.DaySelected = DaySelected
+        self.List = List
+        self.ElectoralDistrict = ElectoralDistrict
+        self.NumberOfVotes =NumberOfVotes 
+        self.ParliamentaryTraineeship = ParliamentaryTraineeship
+        self.Club= Club
+        self.DateAndPlaceOfBirth = DateAndPlaceOfBirth
+        self.Education = Education
+        self.GraduatedSchool = GraduatedSchool
+        self.Occupation = Occupation
+        self.ImageSrc = ImageSrc
+        self.IageArr = ImageArr
